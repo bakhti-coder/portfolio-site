5b11a4e0929efd14fcf99030397db449a0d51ae9
bakhtiyor-creator <bsayfiddinov777@gmail.com>
2023-10-08 18:38:34 +0500

    ok

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import ScrollToTop from "@/components/ScrollToTop";
 import "node_modules/react-modal-video/css/modal-video.css";
-import '../public/styles/index.css'
+import "../public/styles/index.css";
 
 export default function RootLayout({
   children,
--- context/GetContex.jsx
+++ context/GetContex.jsx
@@ -1,28 +1,28 @@
-// import axios from "axios";
-// import { useEffect } from "react";
-// import { useState } from "react";
-// import { createContext } from "react";
+import axios from "axios";
+import { useEffect } from "react";
+import { useState } from "react";
+import { createContext } from "react";
 
-// export const GetDataContex = createContext();
+export const GetDataContex = createContext();
 
-// const GetDataContextProvider = ({ children }) => {
-//   const [post, setPost] = useState([]);
+const GetDataContextProvider = ({ children }) => {
+  const [post, setPost] = useState([]);
 
-//   useEffect(() => {
-//     const getData = async () => {
-//       try {
-//         const { data } = await axios.get("http://10.10.3.74:7777/get_owner");
-//         console.log(data);
-//       } catch (error) {
-//         console.log(error);
-//       }
-//     };
-//   }, []);
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const { data } = await axios.get("http://10.10.3.74:7777/get_owner");
+        console.log(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+  }, []);
 
-//   const state = {};
-//   return (
-//     <GetDataContex.Provider value={state}>{children}</GetDataContex.Provider>
-//   );
-// };
+  const state = {};
+  return (
+    <GetDataContex.Provider value={state}>{children}</GetDataContex.Provider>
+  );
+};
 
-// export default GetDataContextProvider;
+export default GetDataContextProvider;
